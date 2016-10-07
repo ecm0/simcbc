ca55b3c6833343ec409cae0d1ec190c8c9f28f9a
Eric Chassande-Mottin <eric.chassande-mottin@ligo.org>
2016-10-07 07:52:03 +0200

    fixed behaviour

--- createsummary.sh
+++ createsummary.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-for d in `find . -type d -name 'mdc*[0-9]' -maxdepth 1`; do
-    python -W ignore createsummary.py $d/$d.xml $d/coinc.xml 
+for d in `find $1 -maxdepth 1 -type d -name 'mdc*[0-9]'`; do
+    simdir=`basename $d`
+    python -W ignore createsummary.py $d/${simdir}.xml $d/coinc.xml 
 done
