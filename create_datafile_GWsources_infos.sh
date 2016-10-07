f7e97cc9de0720312c39edec3415e9db645abfd9
Eric Chassande-Mottin <eric.chassande-mottin@ligo.org>
2016-10-07 18:05:27 +0200

    allows for scanning a given dir

--- create_datafile_GWsources_infos.sh
+++ create_datafile_GWsources_infos.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-for d in `find . -type d -name 'mdc*[0-9]'`; do
-    python -W ignore create_datafile_GWsources_infos.py $d/$d.xml $d/coinc.xml 
+for d in `find $1 -maxdepth 1 -type d -name 'mdc*[0-9]'`; do
+    simdir=`basename $d`
+    python -W ignore create_datafile_GWsources_infos.py $d/${simdir}.xml $d/coinc.xml 
 done
